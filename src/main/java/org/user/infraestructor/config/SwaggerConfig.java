8065777b7d7a0a270613275343b1231195bc91f7
luchoguerraa <luis.guerra.aedo@gmail.com>
2024-02-15 10:13:47 -0300

    fix url swagger

--- src/main/java/org/user/infraestructor/config/SwaggerConfig.java
+++ src/main/java/org/user/infraestructor/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.user.application.controller"))
+                .apis(RequestHandlerSelectors.basePackage("org.user.infraestructor.web.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
